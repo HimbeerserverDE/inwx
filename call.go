99f68ed6938c81010d64c0d07e0055e7ca54a7c4
HimbeerserverDE <himbeerserverde@gmail.com>
2022-10-15 14:00:48 +0200

    export ErrUnexpectedStatus's fields

--- call.go
+++ call.go
@@ -31,13 +31,13 @@ type Response struct {
 // ErrUnexpectedStatus indicates that a Call was responded to
 // but the Status doesn't match any of the expected Statuses of the Call.
 type ErrUnexpectedStatus struct {
-	expected []Status
-	got      Status
+	Expected []Status
+	Got      Status
 }
 
 func (e *ErrUnexpectedStatus) Error() string {
 	const format = "unexpected status code: expected %v, got %v"
-	return fmt.Sprintf(format, e.expected, e.got)
+	return fmt.Sprintf(format, e.Expected, e.Got)
 }
 
 // Call sends a Call to the API endpoint and waits for a response or an error.
