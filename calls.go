a3d0d5f7544fe38ae76438d46c530ce066664865
HimbeerserverDE <himbeerserverde@gmail.com>
2022-10-16 14:12:47 +0200

    add NSRecordInfoCall

--- calls.go
+++ calls.go
@@ -14,6 +14,21 @@ type logoutCall struct{}
 func (c *logoutCall) method() string           { return "account.logout" }
 func (c *logoutCall) expectedStatus() []Status { return []Status{SuccessClosing} }
 
+// A NSRecordInfoCall returns detailed information about a DNS record.
+type NSRecordInfoCall struct {
+	DomainName string `json:"domain,omitempty"`
+	DomainID   int    `json:"roId,omitempty"`
+	RecordID   int    `json:"recordId,omitempty"`
+	Type       string `json:"type,omitempty"`
+	Name       string `json:"name,omitempty"`
+	Content    string `json:"content,omitempty"`
+	TTL        int    `json:"ttl,omitempty"`
+	Priority   int    `json:"prio,omitempty"`
+}
+
+func (c *NSRecordInfoCall) method() string           { return "nameserver.info" }
+func (c *NSRecordInfoCall) expectedStatus() []Status { return []Status{Success} }
+
 // A NSUpdateRecordsCall updates one or more DNS records.
 type NSUpdateRecordsCall struct {
 	IDs                 []int                 `json:"id"`
