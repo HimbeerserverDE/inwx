1beb30305c5479f0ff1da328772c7a702dc071f3
HimbeerserverDE <himbeerserverde@gmail.com>
2022-10-16 16:52:47 +0200

    record info: slice of records

--- responses.go
+++ responses.go
@@ -2,16 +2,15 @@ package inwx
 
 import "time"
 
-// A NSRecordInfoResponse contains the information that was requested
-// about a specific DNS record.
+// A NSRecordInfoResponse contains all NSRecords that match the search.
 type NSRecordInfoResponse struct {
-	DomainID      int       `json:"roId"`
-	DomainName    string    `json:"domain"`
-	DomainType    NSType    `json:"type"`
-	MasterAddr    string    `json:"masterIp"`
-	LastZoneCheck time.Time `json:"lastZoneCheck"`
-	SlaveDNS      SlaveInfo `json:"slaveDns"`
-	SOASerial     string    `json:"SOAserial"`
-	RecordCount   int       `json:"count"`
-	Record        NSRecord  `json:"record"`
+	DomainID      int        `json:"roId"`
+	DomainName    string     `json:"domain"`
+	DomainType    NSType     `json:"type"`
+	MasterAddr    string     `json:"masterIp"`
+	LastZoneCheck time.Time  `json:"lastZoneCheck"`
+	SlaveDNS      SlaveInfo  `json:"slaveDns"`
+	SOASerial     string     `json:"SOAserial"`
+	RecordCount   int        `json:"count"`
+	Record        []NSRecord `json:"record"`
 }
